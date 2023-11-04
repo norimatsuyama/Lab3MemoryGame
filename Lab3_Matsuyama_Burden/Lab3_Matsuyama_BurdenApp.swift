dbdc48087a6aba64d8990e6566f7912fcd30abbc
Norika <norika.sf@gmail.com>
2023-11-04 12:18:27 -0700

    Initial Commit

---
+++ Lab3_Matsuyama_Burden/Lab3_Matsuyama_BurdenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3_Matsuyama_BurdenApp.swift
+//  Lab3_Matsuyama_Burden
+//
+//  Created by Norika on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Lab3_Matsuyama_BurdenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
